246a34e1127398c63ada6b3639927858f1b9dcf7
pekand <pekand@gmail.com>
2019-10-07 17:35:35 +0200

    js editor

--- js/002-editor/script.js
+++ js/002-editor/script.js
@@ -1,35 +1,35 @@
 function generateBlocks() {
-   var blocks = document.querySelectorAll(".block");
-    blocks.forEach( el => {
+    var blocks = document.querySelectorAll(".block");
+    blocks.forEach(el => {
         var css = el.querySelector('.css').value;
         var html = el.querySelector('.html').value;
         var js = el.querySelector('.js').value;
-        
+
         var out = el.querySelector('.out');
-        
+
         out.innerHTML = "";
-          
+
         var style = document.createElement('style');
         style.innerHTML = css;
         out.appendChild(style);
-        
+
         var script = document.createElement('script');
         script.innerHTML = js;
         out.appendChild(script);
-        
+
         var content = document.createElement('div');
         content.innerHTML = html;
         out.appendChild(content);
-        
 
-    }); 
+
+    });
 }
 
 document.querySelectorAll("textarea").forEach(function(el) {
-    el.addEventListener("change", function(){
-      generateBlocks();
-    }); 
-    
+    el.addEventListener("change", function() {
+        generateBlocks();
+    });
+
     el.value = el.value.trim().replace(/(    |\t)/gi, '');
 });
 
