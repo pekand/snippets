d99f1181fb2c89e3ba993e08278391942cc03c87
pekand <pekand@gmail.com>
2019-12-01 21:26:37 +0100

    unicode table

--- tables/unicode.php
+++ tables/unicode.php
@@ -47,7 +47,7 @@ main div {
 for($j = 0; $j < 64 ; $j++) {
 
 	for($i = $j*(65536/64); $i < (($j+1)*(65536/64))  ; $i++) {
-		echo "<div class='char'><div class='symbol'>".mb_chr($i, "utf8")."</div> <div class='description' title='$i'>U+".strtoupper(str_pad(dechex($i), 5, "0", STR_PAD_LEFT ))."</div></div>";
+		echo "<div class='char'><div class='symbol'>".mb_chr($i, "utf8")."</div> <div class='description' title='$i'>U+".strtoupper(str_pad(dechex($i), 4, "0", STR_PAD_LEFT ))."</div></div>";
 	}
 }
 
