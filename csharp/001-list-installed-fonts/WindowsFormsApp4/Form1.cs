dc2616314b992fadb6ec29847b2fa76a9864211b
pekand <pekand@gmail.com>
2019-12-09 16:42:06 +0100

    list installed fonts

---
+++ csharp/001-list-installed-fonts/WindowsFormsApp4/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            using (InstalledFontCollection fontsCollection = new InstalledFontCollection())
+            {
+                FontFamily[] fontFamilies = fontsCollection.Families;
+                List<string> fonts = new List<string>();
+                foreach (FontFamily font in fontFamilies)
+                {
+                    fonts.Add(font.Name);
+                    richTextBox1.Text += font.Name+"\n";
+                }
+
+            }
+        }
+    }
+}
