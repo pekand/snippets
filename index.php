3e86b654defa8623dab9442652f9cada99aa1aa4
Andrej Pekar <pekand@gmail.com>
2021-01-14 20:30:34 +0100

    index files

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Snippets</title>
+</head>
+<body>
+    <style type="text/css">
+        * {
+            color: black;
+            margin: 0px;
+            padding: 0px;
+            font-size:32px;
+            font-family:monospace;
+        }
+
+        a {
+            text-decoration: none;
+        }
+
+        body {
+            background:#48C0B8;
+        }
+
+        main {
+            padding-top: 200px;
+            width:800px;
+            margin: 0px auto;
+        }
+
+        h1  {
+            margin: 30px 0px;
+        }
+        table {
+            border-collapse: collapse;
+        }
+
+        tr {
+
+        }
+
+        td {
+            width:300px;
+        }
+    </style>
+    <main>
+        <h1>Snippets</h1>
+        <table>
+            
+            <tr>
+                <td><a href="/reference">reference</a></td><td></td>
+            </tr>
+            <tr>
+                <td><a href="/snippets">snippets</a></td><td></td>
+            </tr>
+            <tr>
+                <td><a href="/tables">tables</a></td><td></td>
+            </tr>
+            <tr>
+                <td><a href="/commands">commands</a></td><td></td>
+            </tr>
+        </table>
+    </main>
+</body>
+</html>
