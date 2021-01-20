e394a676945bc9b9d78b6775a9f03f8c95baa53f
Andrej Pekar <pekand@gmail.com>
2021-01-20 16:51:35 +0100

    uid - seed

--- snippets/php/030-uid/uid.php
+++ snippets/php/030-uid/uid.php
@@ -1,11 +1,14 @@
 <?php
 
+mt_srand(random_int(PHP_INT_MIN, PHP_INT_MAX));
+
 function uid($len) { 
     $ch = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
     $out = "";
     for ($i=0; $i < $len; $i++) { 
         $out .= $ch[mt_rand(1, strlen($ch))-1];
     }
+    
     return $out;
 }
 
