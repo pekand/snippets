c04393bd88cc47df7dab8ce3cfb10f53a41d99c0
Andrej Pekar <pekand@gmail.com>
2021-02-17 14:36:43 +0100

    csharp - mutex

---
+++ snippets/csharp/004-mutex/WindowsFormsApp/WindowsFormsApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            /*************************************/
+
+            bool createdNew = false;
+            using (Mutex mutex = new Mutex(true, "APPLICATION_NAME", out createdNew))
+            {
+                if (createdNew)
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new Form1());
+                }
+                else
+                {
+                    MessageBox.Show("Another instance already running!");
+                }
+            }
+
+            /*************************************/
+
+           
+        }
+    }
+}
