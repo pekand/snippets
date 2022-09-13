593fdfd8f92f2ce01d4a66598744e34961330e8f
Andrej Pekar <andrej.pekar@nike.sk>
2022-09-13 14:04:59 +0200

    php - json

---
+++ snippets/php/052-json-pretty/pretty.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$decoded = json_decode(file_get_contents("data.json"), true);
+
+function varexport($expression, $return=FALSE) {
+    if (!is_array($expression)) return var_export($expression, $return);
+    $export = var_export($expression, TRUE);
+    $export = preg_replace("/^([ ]*)(.*)/m", '$1$1$2', $export);
+    $array = preg_split("/\r\n|\n|\r/", $export);
+    $array = preg_replace(["/\s*array\s\($/", "/\)(,)?$/", "/\s=>\s$/"], [NULL, ']$1', ' => ['], $array);
+    $export = join(PHP_EOL, array_filter(["["] + $array));
+    if ((bool)$return) return $export; else echo $export;
+}
+
+echo varexport($decoded, true);
