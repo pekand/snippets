297a72c741abcef23b01d1558f890fa08561a5e8
Andrej Pekar <andrej.pekar@nike.sk>
2022-12-02 13:06:49 +0100

    rdkafka consumer producer example

---
+++ snippets/kafka/001-init/producer.php
@@ -0,0 +1,45 @@
+<?php
+
+require_once "config.php";
+
+use RdKafka\Conf;
+use RdKafka\KafkaConsumer;
+use RdKafka\TopicPartition;
+
+$conf = new RdKafka\Conf();
+$conf->set('bootstrap.servers', Config::$metadataBbrokerList);
+$conf->set('socket.timeout.ms', (string) 50);
+$conf->set('queue.buffering.max.messages', (string) 1000);
+$conf->set('max.in.flight.requests.per.connection', (string) 1);
+$conf->setDrMsgCb(
+    function (\RdKafka\Producer $producer, \RdKafka\Message $message): void {
+        if ($message->err !== RD_KAFKA_RESP_ERR_NO_ERROR) {
+            // Perform your error handling here using $message->errstr()
+        }
+    }
+);
+$conf->set('log_level', (string) LOG_DEBUG);
+$conf->set('debug', 'all');
+$conf->set('statistics.interval.ms', (string) 1000);
+
+
+
+$topicConf = new \RdKafka\TopicConf();
+$topicConf->set('message.timeout.ms', (string) 30000);
+$topicConf->set('request.required.acks', (string) -1);
+$topicConf->set('request.timeout.ms', (string) 5000);
+
+$producer = new \RdKafka\Producer($conf);
+
+$topic = $producer->newTopic('test123', $topicConf);
+
+for ($i = 0; $i < 5; $i++) {
+    $key = $i % 10;
+    $payload = "payload-$i-$key".date("Y-m-d h:i:s");
+    $topic->produce(RD_KAFKA_PARTITION_UA, 0, $payload, (string) $key);
+
+    // trigger callback queues
+    $producer->poll(1);
+}
+
+$producer->flush(5000);
---
+++ snippets/kafka/001-init/readme.txt
@@ -0,0 +1,5 @@
+RdKafka - php extension
+
+https://www.baeldung.com/ops/kafka-docker-setup
+
+kafka-topics --create --topic test123 --partitions 1  --replication-factor 1 --bootstrap-server :9092
