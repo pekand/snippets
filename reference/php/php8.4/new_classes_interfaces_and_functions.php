e29f3b6ee761b9d1af9e859c2d5286aba6a9c140
Pekar Andrej <pekand@gmail.com>
2024-12-04 10:32:51 +0100

    php - reference

---
+++ reference/php/php8.4/new_classes_interfaces_and_functions.php
@@ -0,0 +1,15 @@
+<?php
+
+//New Lazy Objects.
+//New JIT implementation based on IR Framework.
+//New request_parse_body() function.
+//New bcceil(), bcdivmod(), bcfloor(), and bcround() functions.
+//New RoundingMode enum for round() with 4 new rounding modes TowardsZero, AwayFromZero, NegativeInfinity, and PositiveInfinity.
+//New DateTime::createFromTimestamp(), DateTime::getMicrosecond(), DateTime::setMicrosecond(), DateTimeImmutable::createFromTimestamp(), DateTimeImmutable::getMicrosecond(), and DateTimeImmutable::setMicrosecond() methods.
+//New mb_trim(), mb_ltrim(), mb_rtrim(), mb_ucfirst(), and mb_lcfirst() functions.
+//New pcntl_getcpu(), pcntl_getcpuaffinity(), pcntl_getqos_class(), pcntl_setns(), and pcntl_waitid() functions.
+//New ReflectionClassConstant::isDeprecated(), ReflectionGenerator::isClosed(), and ReflectionProperty::isDynamic() methods.
+//New http_get_last_response_headers(), http_clear_last_response_headers(), and fpow() functions.
+//New XMLReader::fromStream(), XMLReader::fromUri(), XMLReader::fromString(), XMLWriter::toStream(), XMLWriter::toUri(), and XMLWriter::toMemory() methods.
+//New grapheme_str_split() function.
+
